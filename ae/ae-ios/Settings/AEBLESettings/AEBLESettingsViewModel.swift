37c9efd2a9e83be91ae486dd2282c5c3a07df610
Blaine Rothroc <blaine.rothrock@protonmail.com>
2022-04-17 23:38:37 -0500

    allow http

--- ae/ae-ios/Settings/AEBLESettings/AEBLESettingsViewModel.swift
+++ ae/ae-ios/Settings/AEBLESettings/AEBLESettingsViewModel.swift
@@ -70,7 +70,7 @@ import aeble
         }
         
         if isRemoteServerEnabled {
-            aeble.settings.sensorDataBucketName =  bucketNames[bucketNameIndex]
+            aeble.settings.sensorDataBucketName = bucketNames[bucketNameIndex]
         }
         
         aeble.settings.apiURL = url
